5f0e0b06fba738252f88bc2c26fae4130b53402b
Maxim Panfilov <Maxim.Panfilov@acronis.com>
2024-10-09 14:23:32 +0300

    fix tests and lint

--- example.go
+++ example.go
@@ -10,6 +10,10 @@ import (
 	"github.com/acronis/go-stacktrace"
 )
 
+const (
+	ExampleValue = "value"
+)
+
 var ErrValueKeyNotFound = errors.New("value key not found")
 
 func (ex *Example) decode(node *yaml.Node, valueNode *yaml.Node, location string) error {
@@ -44,7 +48,7 @@ func (ex *Example) fill(location string, value *yaml.Node) error {
 	for i := 0; i != len(value.Content); i += 2 {
 		node := value.Content[i]
 		valueNode := value.Content[i+1]
-		if node.Value == "value" {
+		if node.Value == ExampleValue {
 			valueKey = valueNode
 			break
 		}
